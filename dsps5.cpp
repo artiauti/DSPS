195bee9d697d4fb0be76d4aa616971102f1f55e1
artiauti <161191564+artiauti@users.noreply.github.com>
2024-09-26 19:24:39 +0530

    Add files via upload

---
+++ dsps5.cpp
@@ -0,0 +1,57 @@
+/*Arti Auti, Roll no.:05,dsps assignment 5,date:12-9-24*/
+#include<iostream>
+#include<string>
+#include<stack>
+using namespace std;
+int precedence(char c)
+{
+    if(c == '^')
+        return 3;
+    else if(c == '/' || c == '*')
+        return 2;
+    else if(c == '-' || c == '+')
+        return 1;
+    else
+        return -1; 
+} 
+string InfixToPostfix(string exp)
+{
+    stack<char> s;
+    string res;
+    for(int i = 0; i < exp.length(); i++)
+    {
+
+        if((exp[i] >= 'a' && exp[i] <= 'z') || (exp[i] >= 'A' && exp[i] <= 'Z'))
+            res += exp[i];
+       
+        else if(exp[i] == '(')
+            s.push(exp[i]);
+
+        else if(exp[i] == ')')
+        {
+            while(!s.empty() && s.top() != '(') {
+                res += s.top();
+                s.pop();
+            }
+            s.pop(); 
+        } 
+        else
+        {
+            while (!s.empty() && precedence(s.top()) >= precedence(exp[i])) {
+                res += s.top();
+                s.pop();
+            }
+            s.push(exp[i]); 
+        }
+    }
+    while(!s.empty()) {
+        res += s.top();
+        s.pop();
+    }
+    return res;
+}
+int main()
+{
+    string s = "(a-b/c)*(a/k-l)";
+    cout << InfixToPostfix(s) << endl;
+}
